461cd7ae4dd1be6e6577d61c9b1e2a87ba870976
Mayur Dusane <mayurdusane1@gmail.com>
2017-03-12 21:45:22 +0530

    updated

--- php/contact.php
+++ php/contact.php
@@ -39,7 +39,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@example.net";
-$address = "mayurdusane1@gmail.com";
+$address = "info@mayurdusane.me";
 
 
 // Configuration option.
